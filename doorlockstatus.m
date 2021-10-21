7c5b60dba7dfad34ffdcf898952fed66cc8bde9a
sagarkudkelwar <91977216+sagarkudkelwar@users.noreply.github.com>
2021-10-21 21:42:26 +0530

    Add files via upload
    Updated model uploaded for door lock system

---
+++ doorlockstatus.m
@@ -0,0 +1,7 @@
+classdef doorlockstatus<Simulink.IntEnumType
+    enumeration
+        door_lock_open(0)
+        door_lock_close(1)
+        
+    end
+end
---
+++ taillight.m
@@ -0,0 +1,6 @@
+classdef taillight<Simulink.IntEnumType
+    enumeration
+        tail_light_on(1)
+        tail_light_off(0)
+    end
+end
