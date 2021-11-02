c2ac7eac774fefedee2b7ca6f4bef6a10ca8f5da
Pravin S Khade <87241938+PK1469@users.noreply.github.com>
2021-11-02 17:23:54 +0530

    Add files via upload
    Sagar Sir File

--- carlight.m
+++ carlight.m
@@ -1,6 +1,6 @@
 classdef carlight<Simulink.IntEnumType
     enumeration
-        Head_light_on(1)
-        Head_light_off(0)
+        Head_light_tail_light_on(1)
+        Head_light_tail_light_off(0)
     end
 end
